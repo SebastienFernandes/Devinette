917dea9ee6a9a62999871c88c35e5cbe18d6818e
Seb <fernandes91seb@gmail.com>
2017-01-16 17:50:31 +0100

    Ajout en local du contenu html et js pour le dossier devinette

---
+++ js/devinette.js
@@ -0,0 +1,38 @@
+/* 
+Activité : jeu de devinette
+*/
+
+// NE PAS MODIFIER OU SUPPRIMER LES LIGNES CI-DESSOUS
+// COMPLETEZ LE PROGRAMME UNIQUEMENT APRES LE TODO
+
+console.log("Bienvenue dans ce jeu de devinette !");
+
+// Cette ligne génère aléatoirement un nombre entre 1 et 100
+var solution = Math.floor(Math.random() * 100) + 1;
+
+// Décommentez temporairement cette ligne pour mieux vérifier le programme
+console.log("(La solution est " + solution + ")");
+
+
+for (var i=1; i<=6; i++){
+    var nb = Number(prompt("saisir la valeur de votre choix :"));
+
+    while (isNaN(nb)){
+        console.log("la valeur demandée doit etre un chiffre ou un nombre! ");
+        nb = Number(prompt());
+    }
+    
+    if (nb < solution){
+        console.log(nb+" est trop petit");
+    } else if (nb > solution){
+        console.log(nb+" est trop grand");
+    } else if (nb === solution){
+        console.log("Bravo! la solution etait "+nb);
+        console.log("vous avez trouvé la reponse en "+ i +" essai(s)! Bien joué.");
+        i = 6;
+    }
+}
+
+if ((i === 7) & (nb !== solution)){
+    console.log("perdu... La solution etait "+ solution);    
+}
